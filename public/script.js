221bea3af1af25bca9415d90995c7edb7d9463d7
505226 <charlie5charlie@outlook.com>
2026-02-16 17:07:18 +0000

    fixed colour issue and incorrect team stat data.

--- public/script.js
+++ public/script.js
@@ -274,7 +274,7 @@ function updateLeaderboards(season, gameKey) {
   const sortedTeams = [...season.team_leaderboard].sort((a, b) => (b[gameKey] || 0) - (a[gameKey] || 0));
   sortedTeams.forEach((team, index) => {
     // Get team color if you have it
-    const teamInfo = sortedTeams.find(t => t.team_name === team.team_name);
+    const teamInfo = teams.find(t => t.team_name === team.team_name);
     const teamcolor = teamInfo ? teamInfo.team_color : "white";
 
     // Generate player images for this team
@@ -296,7 +296,7 @@ function updateLeaderboards(season, gameKey) {
   // Sort players by selected game points descending
   const sortedPlayers = [...season.player_leaderboard].sort((a, b) => (b[gameKey] || 0) - (a[gameKey] || 0));
   sortedPlayers.forEach((player, index) => {
-    const teamInfo = sortedTeams.find(t => t.team_name === player.team);
+    const teamInfo = teams.find(t => t.team_name === player.team);
     const teamcolor = teamInfo ? teamInfo.team_color : "white";
 
     const row = document.createElement("div");
