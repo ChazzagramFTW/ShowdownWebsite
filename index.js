07f35db7b4686cb8e4886db5fca3c0b86dc33efb
505226 <charlie5charlie@outlook.com>
2026-04-26 01:11:20 +0100

    Removed 404 for testing.

--- index.js
+++ index.js
@@ -223,12 +223,4 @@ async function startServer() {
     }
 }
 
-app.use((req, res) => {
-  if (req.path.startsWith('/api/')) {
-    return res.status(404).json({ error: 'Not found' });
-  }
-
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
-
 startServer();
