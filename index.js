cbe15f55b1535360758e92b15cf30a3599e24d20
505226 <charlie5charlie@outlook.com>
2026-04-26 01:08:17 +0100

    Updated 404

--- index.js
+++ index.js
@@ -224,6 +224,10 @@ async function startServer() {
 }
 
 app.use((req, res) => {
+  if (req.path.startsWith('/api/')) {
+    return res.status(404).json({ error: 'Not found' });
+  }
+
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
